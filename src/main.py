3cefd7d52c437fe3f33a35c377f2f057544fcaa0
Chrisc-80 <chrisc@wp.pl>
2025-05-13 10:51:29 +0200

    I: update to main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+	node = TextNode("This is some anchore text", TextType.LINK, "https://www.boot.dev")
+	print(node)
+
+
+main()
