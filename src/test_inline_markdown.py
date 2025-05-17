cd1826ab8cdbde2bb5fb0f7e72377708de14c020
Chrisc-80 <chrisc@wp.pl>
2025-05-17 19:59:07 +0200

    W1: changes in test

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -59,8 +59,7 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             split_nodes_delimiter([node], "`", TextType.CODE)
 
     def test_extract_markdown_images(self):
-        matches = extract_markdown_images(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
 if __name__ == "__main__":
