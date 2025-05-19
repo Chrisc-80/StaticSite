b80b747c1578ae2dcf4b6d0f33114da6764c92c7
Chrisc-80 <chrisc@wp.pl>
2025-05-19 11:25:02 +0200

    Y3: bugs removed next test

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -130,7 +130,7 @@ class TestSplitNodesDelimiter(unittest.TestCase):
 		result = text_to_textnodes(
 			"This is **text** with an _italic_ word and a `code block`"
 			"and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-			)
+		)
 		expected = [
     	TextNode("This is ", TextType.TEXT),
     	TextNode("text", TextType.BOLD),
