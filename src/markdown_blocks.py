11e0734be4389688710f17ac8c06cf42ef6ec383
Chrisc-80 <chrisc@wp.pl>
2025-05-20 13:00:27 +0200

    Ab9: another bug fixed ...

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,6 @@
 from enum import Enum
-
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, text_node_to_html_node
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
