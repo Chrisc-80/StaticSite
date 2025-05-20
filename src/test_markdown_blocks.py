e7457f41f1a9adefa551e9127ac67b7f5f7a5f64
Chrisc-80 <chrisc@wp.pl>
2025-05-20 12:17:11 +0200

    Ab8: Last time check

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType, markdown_to_html_node
 
 
 class TestMarkdownToHTML(unittest.TestCase):
