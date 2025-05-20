8bfeb48be7816b490907f9c85e1a9f08665a4b74
Chrisc-80 <chrisc@wp.pl>
2025-05-20 11:29:18 +0200

    Ab4: Still bugs in test file

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type, markdown_to_html_node
-
+from htmlnode import ParentNode
 
 class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_to_blocks(self):
