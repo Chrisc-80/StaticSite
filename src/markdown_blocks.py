2d0a400d4121248f9f1fee6334aa95e9061a5804
Chrisc-80 <chrisc@wp.pl>
2025-05-21 09:35:27 +0200

    Ab14: maby last small bug fixed

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -29,7 +29,7 @@ def block_to_block_type(block):
     if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
         return BlockType.HEADING
     if len(lines) > 1 and lines[0].startswith("```") and (lines[-1] == "```" or lines[-1].startswith("```")):
-    return BlockType.CODE
+        return BlockType.CODE
     if block.startswith(">"):
         for line in lines:
             if not line.startswith(">"):
