e0b81a5ae5378e2fecb33104c3298e96d0aedb1f
Chrisc-80 <chrisc@wp.pl>
2025-05-23 11:49:03 +0200

    Ai: adding copystatic...

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+from copystatic import copy_files_recursive
 from gencontent import generate_page
 
 dir_path_static = "./static"
@@ -8,40 +9,21 @@ dir_path_public = "./public"
 dir_path_content = "./content"
 template_path = "./template.html"
 
-def copy_files_recursive(dir_path_static, dir_path_public):
-
-	if not os.path.exists(dir_path_public):
-		# create public
-		os.mkdir(dir_path_public)
-
-	#copy static to public
-	items = os.listdir(dir_path_static)
-	for item in items:
-		item_path = os.path.join(dir_path_static, item)
-		dest_path = os.path.join(dir_path_public, item)
-		if os.path.isfile(item_path):
-			shutil.copy(item_path, dest_path)
-			
-		else:
-			os.mkdir(dest_path)
-			copy_files_recursive(item_path, dest_path)
 
 def main():
-
-	# del public if it exist
-	print("Deleting public directory...")
+    print("Deleting public directory...")
     if os.path.exists(dir_path_public):
-		shutil.rmtree(dir_path_public)
-	
-	print("Copying static files to public directory...")
-	copy_files_recursive(dir_path_static, dir_path_public)
+        shutil.rmtree(dir_path_public)
 
-	print("Generating page...")
-	generate_page(
-		os.path.join(dir_path_content, "index.md"),
-		template_path,
-		os.path.join(dir_path_public, "index.html"),
-	)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 
 main()
