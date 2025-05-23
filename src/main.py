c5034536c5d93a6597d01f93c54617c77fd26c15
Chrisc-80 <chrisc@wp.pl>
2025-05-23 10:57:14 +0200

    Ae: add procedure to create public dict

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
+import os, shutil
 from textnode import TextNode, TextType
 
+def copy_static(source_dir, dest_dir):
+
+	# del public if it exist
+	if os.path.exists(dest_dir):
+		shutil.rmtree(dest_dir)
+		
+	# create public
+	os.mkdir(dest_dir)
+	
+	#copy static to public
+	items = os.listdir(source_dir)
+	for item in items:
+		item_path = os.path.join(source_dir, item)
+		dest_path = os.path.join(dest_dir, item)
+		if os.path.isfile(item_path):
+			shutil.copy(item_path, dest_path)
+			
+		else:
+			os.mkdir(dest_path)
+			copy_static(item_path, dest_path)
+		print(item_path, dest_path)
+
 def main():
-	node = TextNode("This is some anchore text", TextType.LINK, "https://www.boot.dev")
-	print(node)
+    copy_static("static", "public")
+	
 
 
 main()
